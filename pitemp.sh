d580c4bb1f358faadc2575362b850b1bc202601e
Speedy905 <AK.Mijares99@gmail.com>
2018-08-22 09:39:40 -0400

    Fixed formatting

--- pitemp.sh
+++ pitemp.sh
@@ -8,4 +8,4 @@ cpu=$(</sys/class/thermal/thermal_zone0/temp)
 echo "$(date) @ $(hostname)"
 echo "--------------------"
 echo "GPU $(/opt/vc/bin/vcgencmd measure_temp)"
-echo "CPU = $((cpu/1000)) 'C"
+echo "CPU temp=$((cpu/1000))'C"
