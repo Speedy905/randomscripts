62cb2f88a15fea3f8545ad280a8d3a88afa95472
Speedy905 <AK.Mijares99@gmail.com>
2018-08-22 09:29:26 -0400

    Initial commit

---
+++ pitemp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Pi temps
+
+cpu=$(</sys/class/thermal/thermal_zone0/temp)
+echo "$(date) @ $(hostname)"
+echo "--------------------"
+echo "GPU $(/opt/vc/bin/vcgencmd measure_temp)"
+echo "CPU = $((cpu/1000)) 'C"
