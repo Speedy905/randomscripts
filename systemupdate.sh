bb6464af5e1fddd4b3fa1c9f2c5c386238176b1c
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2018-11-15 09:45:22 -0500

    Removed checking date condition

--- systemupdate.sh
+++ systemupdate.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-currentday=`date '+%d'` 
-today=$(date)
-
-checkinternetandupdate(){
 echo 'Checking for an internet connection. Please wait.'
 ping -q -c5 8.8.8.8 > /dev/null
 if [ $? -eq 0 ]
@@ -15,12 +11,5 @@ else
     echo 'Cannot connect to the internet.'
     exit 1
 fi
-}
 
-if [ $currentday == 01 ]
-then
-    echo 'Today is '$today. 'Doing system update.'
-    checkinternetandupdate
-else
-    echo 'Not the first day of the month. Will not be doing system update'
-fi
+
